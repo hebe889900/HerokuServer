b8daabcc7803ef0434f1256ef8f323b472920b76
Jack <jackyan63@gmail.com>
2016-02-15 06:38:12 -0500

    Second

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var http = require('http');
-http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
